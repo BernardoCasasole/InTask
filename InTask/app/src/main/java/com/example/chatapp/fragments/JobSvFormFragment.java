2e9f1bd17e9c8833a9d6932d5aae0bcae36c70e4
ChristianCammareri <christian.cammareri@mail.polimi.it>
2020-08-22 00:49:18 +0200

    Upload posizione

--- InTask/app/src/main/java/com/example/chatapp/fragments/JobSvFormFragment.java
+++ InTask/app/src/main/java/com/example/chatapp/fragments/JobSvFormFragment.java
@@ -1,7 +1,14 @@
 package com.example.chatapp.fragments;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -14,8 +21,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.chatapp.R;
@@ -28,7 +38,11 @@ import com.google.firebase.storage.StorageReference;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+
+import static android.content.Context.LOCATION_SERVICE;
 
 
 public class JobSvFormFragment extends Fragment {
@@ -50,11 +64,37 @@ public class JobSvFormFragment extends Fragment {
     private static Bitmap Image = null;
     private static final int GALLERY = 1;
 
+    double latitude, longitude;
+    TextView getPosition;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         rootView = inflater.inflate(R.layout.fragment_job_sv_form, container, false);
+
+        locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+
+            }
+        };
+
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION,
+                    Manifest.permission.INTERNET}, 10);
+
+        }
+        locationManager.requestLocationUpdates("gps", 0, 0, locationListener);
+
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference("Job");
@@ -87,10 +127,37 @@ public class JobSvFormFragment extends Fragment {
             }
         });
 
+        getPosition = rootView.findViewById(R.id.get_position);
+        getPosition.setClickable(true);
+        getPosition.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                Geocoder geocoder;
+                List<Address> addresses;
+                geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+
+                try {
+                    addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+                    String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                    String[] parts = address.split(",");
+                    street.setText(parts[0]);
+                    houseNumber.setText(parts[1]);
+                    location.setText(parts[2]);
+                } catch (IOException | IndexOutOfBoundsException e) {
+                    Toast.makeText(getContext(), "Indirizzo non trovato", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
 
         btn_publish = rootView.findViewById(R.id.proposal_btn_publish);
-
-
         btn_publish.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
--- InTask/app/src/main/java/com/example/chatapp/fragments/PublicationTimeFragment.java
+++ InTask/app/src/main/java/com/example/chatapp/fragments/PublicationTimeFragment.java
@@ -1,9 +1,19 @@
 package com.example.chatapp.fragments;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.media.Image;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
@@ -15,9 +25,11 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.example.chatapp.ProfileActivity;
@@ -34,11 +46,15 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
+
+import static android.content.Context.LOCATION_SERVICE;
 
 public class PublicationTimeFragment extends Fragment {
 
-    EditText title, description, day, time, distance,location, houseNumber, street;
+    EditText title, description, day, time, distance, location, houseNumber, street;
     ImageView imageView;
     RadioGroup type;
     RadioButton radioButton, verified;
@@ -55,11 +71,35 @@ public class PublicationTimeFragment extends Fragment {
     private static Bitmap Image = null;
     private static final int GALLERY = 1;
 
+    double latitude, longitude;
+    TextView getPosition;
+    LocationManager locationManager;
+    LocationListener locationListener;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         rootView = inflater.inflate(R.layout.fragment_publication_time, container, false);
+
+        locationManager = (LocationManager) getActivity().getSystemService(LOCATION_SERVICE);
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+
+            }
+        };
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                requestPermissions(new String[]{
+                        Manifest.permission.ACCESS_FINE_LOCATION,
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.INTERNET}, 10);
+
+        }
+            locationManager.requestLocationUpdates("gps", 0, 0, locationListener);
+
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
         databaseReference = FirebaseDatabase.getInstance().getReference("Time");
@@ -74,6 +114,36 @@ public class PublicationTimeFragment extends Fragment {
         time = rootView.findViewById(R.id.time);
         distance = rootView.findViewById(R.id.distance);
         type = rootView.findViewById(R.id.type);
+        getPosition = rootView.findViewById(R.id.get_position);
+        getPosition.setClickable(true);
+
+        getPosition.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+
+                Geocoder geocoder;
+                List<Address> addresses;
+                geocoder = new Geocoder(getContext(), Locale.getDefault());
+
+
+                try {
+                    addresses = geocoder.getFromLocation(latitude, longitude, 1); // Here 1 represent max location result to returned, by documents it recommended 1 to 5
+
+                    String address = addresses.get(0).getAddressLine(0); // If any additional address line present than only, check with max available address lines by getMaxAddressLineIndex()
+                    String[] parts = address.split(",");
+                    street.setText(parts[0]);
+                    houseNumber.setText(parts[1]);
+                    location.setText(parts[2]);
+                } catch (IOException | IndexOutOfBoundsException e) {
+                    Toast.makeText(getContext(), "Indirizzo non trovato", Toast.LENGTH_SHORT).show();
+                }
+
+
+            }
+        });
+
 
         imageView = rootView.findViewById(R.id.image);
         imageView.setClickable(true);
@@ -111,17 +181,17 @@ public class PublicationTimeFragment extends Fragment {
                 street_text = street.getText().toString();
                 day_text = day.getText().toString();
                 time_text = time.getText().toString();
-                distance_text =  distance.getText().toString();
+                distance_text = distance.getText().toString();
 
-                if( !title_text.equals("")  &&
-                !description_text.equals("") &&
-                !location_text.equals("") &&
-                !houseNumber_text.equals("") &&
-                !street_text.equals("") &&
-                !day_text.equals("") &&
-                !time_text.equals("") &&
-                !distance_text.equals("") &&
-                radioButton !=null) {
+                if (!title_text.equals("") &&
+                        !description_text.equals("") &&
+                        !location_text.equals("") &&
+                        !houseNumber_text.equals("") &&
+                        !street_text.equals("") &&
+                        !day_text.equals("") &&
+                        !time_text.equals("") &&
+                        !distance_text.equals("") &&
+                        radioButton != null) {
                     Map<String, Object> map = new HashMap<>();
                     map.put("author", firebaseUser.getUid());
                     map.put("title", title_text);
@@ -134,19 +204,19 @@ public class PublicationTimeFragment extends Fragment {
                     map.put("distance", distance_text);
                     map.put("type", radioButton.getText().toString());
                     map.put("verified", verified.isChecked());
-                    map.put("setted_image",uploaded);
-                    Toast.makeText(getContext(),"Annuncio pubblicato con successo",Toast.LENGTH_SHORT).show();
-                if(uploaded){
+                    map.put("setted_image", uploaded);
+                    Toast.makeText(getContext(), "Annuncio pubblicato con successo", Toast.LENGTH_SHORT).show();
+                    if (uploaded) {
 
-                    StorageReference childRef = storageReference.child("/time_images/"+databaseReference.push().getKey()+".jpg");
-                    childRef.putFile(mImageUri);
-                }
+                        StorageReference childRef = storageReference.child("/time_images/" + databaseReference.push().getKey() + ".jpg");
+                        childRef.putFile(mImageUri);
+                    }
                     databaseReference.push().setValue(map);
 
                     getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PublicationChoiceFragment()).commit();
 
-                }else
-                    Toast.makeText(getContext(),"Devi riempire tutti i campi",Toast.LENGTH_SHORT).show();
+                } else
+                    Toast.makeText(getContext(), "Devi riempire tutti i campi", Toast.LENGTH_SHORT).show();
 
 
             }
@@ -156,10 +226,10 @@ public class PublicationTimeFragment extends Fragment {
     }
 
     @Override
-    public void onActivityResult(int requestCode, int resultCode,  Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == GALLERY && resultCode != 0 ) {
+        if (requestCode == GALLERY && resultCode != 0) {
             mImageUri = data.getData();
 
             try {
@@ -171,4 +241,5 @@ public class PublicationTimeFragment extends Fragment {
             }
         }
     }
+
 }
