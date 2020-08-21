be2fb2c4b01aa1459287e5f60ddaee40a166d7e2
ChristianCammareri <christian.cammareri@mail.polimi.it>
2020-08-21 17:38:17 +0200

    Upload immagini in annunci

--- InTask/app/src/main/java/com/example/chatapp/fragments/JobSvFormFragment.java
+++ InTask/app/src/main/java/com/example/chatapp/fragments/JobSvFormFragment.java
@@ -1,12 +1,17 @@
 package com.example.chatapp.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -18,7 +23,10 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -27,12 +35,20 @@ public class JobSvFormFragment extends Fragment {
 
     EditText title, description, day, time, duration,location, houseNumber, street;
     RadioGroup type;
+    ImageView imageView;
     RadioButton radioButton, verified;
     Button btn_publish;
     View rootView;
 
+    Uri mImageUri;
+    Boolean uploaded;
+
     FirebaseUser firebaseUser;
     DatabaseReference databaseReference;
+    StorageReference storageReference;
+
+    private static Bitmap Image = null;
+    private static final int GALLERY = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -41,7 +57,9 @@ public class JobSvFormFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_job_sv_form, container, false);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        databaseReference = FirebaseDatabase.getInstance().getReference("Ads");
+        databaseReference = FirebaseDatabase.getInstance().getReference("Job");
+        storageReference = FirebaseStorage.getInstance().getReference();
+
 
         title = rootView.findViewById(R.id.proposal_title);
         description = rootView.findViewById(R.id.proposal_description);
@@ -52,6 +70,22 @@ public class JobSvFormFragment extends Fragment {
         time = rootView.findViewById(R.id.proposal_time);
         duration = rootView.findViewById(R.id.proposal_duration);
         type = rootView.findViewById(R.id.proposal_type);
+        imageView = rootView.findViewById(R.id.image);
+        imageView.setClickable(true);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imageView.setImageBitmap(null);
+                if (Image != null)
+                    Image.recycle();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), GALLERY);
+
+            }
+        });
 
 
         btn_publish = rootView.findViewById(R.id.proposal_btn_publish);
@@ -64,20 +98,51 @@ public class JobSvFormFragment extends Fragment {
                 radioButton = rootView.findViewById(type.getCheckedRadioButtonId());
                 verified = rootView.findViewById(R.id.proposal_verified);
 
+                String title_text, description_text, day_text, time_text, duration_text, location_text, houseNumber_text, street_text;
+
+                title_text = title.getText().toString();
+                description_text = description.getText().toString();
+                location_text = location.getText().toString();
+                houseNumber_text = houseNumber.getText().toString();
+                street_text = street.getText().toString();
+                day_text = day.getText().toString();
+                time_text = time.getText().toString();
+                duration_text =  duration.getText().toString();
+
+                if( !title_text.equals("")  &&
+                        !description_text.equals("") &&
+                        !location_text.equals("") &&
+                        !houseNumber_text.equals("") &&
+                        !street_text.equals("") &&
+                        !day_text.equals("") &&
+                        !time_text.equals("") &&
+                        !duration_text.equals("") &&
+                        radioButton !=null) {
                 Map<String, Object> map = new HashMap<>();
-                map.put("user", firebaseUser.getUid());
-                map.put("title", title.getText().toString());
-                map.put("description", description.getText().toString());
-                map.put("location", location.getText().toString());
-                map.put("houseNumber", houseNumber.getText().toString());
-                map.put("street", street.getText().toString());
-                map.put("day", day.getText().toString());
-                map.put("time", time.getText().toString());
-                map.put("duration", duration.getText().toString());
+                map.put("author", firebaseUser.getUid());
+                map.put("title", title_text);
+                map.put("description", description_text);
+                map.put("location", location_text);
+                map.put("houseNumber", houseNumber_text);
+                map.put("street", street_text);
+                map.put("day", day_text);
+                map.put("time", time_text);
+                map.put("duration", duration_text);
                 map.put("type", radioButton.getText().toString());
                 map.put("verified", verified.isChecked());
+                map.put("setted_image",uploaded);
+                    Toast.makeText(getContext(),"Annuncio pubblicato con successo",Toast.LENGTH_SHORT).show();
+                    if(uploaded){
 
-                databaseReference.push().setValue(map);
+                        StorageReference childRef = storageReference.child("/job_images/"+databaseReference.push().getKey()+".jpg");
+                        childRef.putFile(mImageUri);
+                    }
+                    databaseReference.push().setValue(map);
+
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PublicationChoiceFragment()).commit();
+
+                }else
+                    Toast.makeText(getContext(),"Devi riempire tutti i campi",Toast.LENGTH_SHORT).show();
 
 
 
@@ -85,5 +150,22 @@ public class JobSvFormFragment extends Fragment {
         });
 
         return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY && resultCode != 0 ) {
+            mImageUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), mImageUri);
+                imageView.setImageBitmap(bitmap);
+                uploaded = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- InTask/app/src/main/java/com/example/chatapp/fragments/PublicationTimeFragment.java
+++ InTask/app/src/main/java/com/example/chatapp/fragments/PublicationTimeFragment.java
@@ -1,35 +1,59 @@
 package com.example.chatapp.fragments;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.media.Image;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import com.example.chatapp.ProfileActivity;
 import com.example.chatapp.R;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PublicationTimeFragment extends Fragment {
 
     EditText title, description, day, time, distance,location, houseNumber, street;
+    ImageView imageView;
     RadioGroup type;
     RadioButton radioButton, verified;
     Button btn_publish;
+    Uri mImageUri;
+    Boolean uploaded;
+
     View rootView;
 
     FirebaseUser firebaseUser;
     DatabaseReference databaseReference;
+    StorageReference storageReference;
+
+    private static Bitmap Image = null;
+    private static final int GALLERY = 1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -38,7 +62,8 @@ public class PublicationTimeFragment extends Fragment {
         rootView = inflater.inflate(R.layout.fragment_publication_time, container, false);
 
         firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-        databaseReference = FirebaseDatabase.getInstance().getReference("Ads1");
+        databaseReference = FirebaseDatabase.getInstance().getReference("Time");
+        storageReference = FirebaseStorage.getInstance().getReference();
 
         title = rootView.findViewById(R.id.title);
         description = rootView.findViewById(R.id.description);
@@ -50,9 +75,24 @@ public class PublicationTimeFragment extends Fragment {
         distance = rootView.findViewById(R.id.distance);
         type = rootView.findViewById(R.id.type);
 
+        imageView = rootView.findViewById(R.id.image);
+        imageView.setClickable(true);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imageView.setImageBitmap(null);
+                if (Image != null)
+                    Image.recycle();
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), GALLERY);
+
+            }
+        });
 
         btn_publish = rootView.findViewById(R.id.btn_publish);
-
 
         btn_publish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,26 +101,74 @@ public class PublicationTimeFragment extends Fragment {
                 radioButton = rootView.findViewById(type.getCheckedRadioButtonId());
                 verified = rootView.findViewById(R.id.verified);
 
-                Map<String, Object> map = new HashMap<>();
-                map.put("user", firebaseUser.getUid());
-                map.put("title", title.getText().toString());
-                map.put("description", description.getText().toString());
-                map.put("location", location.getText().toString());
-                map.put("houseNumber", houseNumber.getText().toString());
-                map.put("street", street.getText().toString());
-                map.put("day", day.getText().toString());
-                map.put("time", time.getText().toString());
-                map.put("duration", distance.getText().toString());
-                map.put("type", radioButton.getText().toString());
-                map.put("verified", verified.isChecked());
+                String title_text, description_text, day_text, time_text, distance_text, location_text, houseNumber_text, street_text;
 
-                databaseReference.push().setValue(map);
 
+                title_text = title.getText().toString();
+                description_text = description.getText().toString();
+                location_text = location.getText().toString();
+                houseNumber_text = houseNumber.getText().toString();
+                street_text = street.getText().toString();
+                day_text = day.getText().toString();
+                time_text = time.getText().toString();
+                distance_text =  distance.getText().toString();
+
+                if( !title_text.equals("")  &&
+                !description_text.equals("") &&
+                !location_text.equals("") &&
+                !houseNumber_text.equals("") &&
+                !street_text.equals("") &&
+                !day_text.equals("") &&
+                !time_text.equals("") &&
+                !distance_text.equals("") &&
+                radioButton !=null) {
+                    Map<String, Object> map = new HashMap<>();
+                    map.put("author", firebaseUser.getUid());
+                    map.put("title", title_text);
+                    map.put("description", description_text);
+                    map.put("location", location_text);
+                    map.put("houseNumber", houseNumber_text);
+                    map.put("street", street_text);
+                    map.put("day", day_text);
+                    map.put("time", time_text);
+                    map.put("distance", distance_text);
+                    map.put("type", radioButton.getText().toString());
+                    map.put("verified", verified.isChecked());
+                    map.put("setted_image",uploaded);
+                    Toast.makeText(getContext(),"Annuncio pubblicato con successo",Toast.LENGTH_SHORT).show();
+                if(uploaded){
+
+                    StorageReference childRef = storageReference.child("/time_images/"+databaseReference.push().getKey()+".jpg");
+                    childRef.putFile(mImageUri);
+                }
+                    databaseReference.push().setValue(map);
+
+                    getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PublicationChoiceFragment()).commit();
+
+                }else
+                    Toast.makeText(getContext(),"Devi riempire tutti i campi",Toast.LENGTH_SHORT).show();
 
 
             }
         });
 
         return rootView;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,  Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == GALLERY && resultCode != 0 ) {
+            mImageUri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), mImageUri);
+                imageView.setImageBitmap(bitmap);
+                uploaded = true;
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
